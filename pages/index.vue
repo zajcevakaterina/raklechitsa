d9c0b9fb59613feff0779c1ac96891298ce9670f
stivman <gisma87@yandex.ru>
2020-05-09 20:03:08 +0700

    комит

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,10 @@
-<template>
-  <div class="container">
-    <!--    <panel class="container__panel"> </panel>-->
-    <video__container></video__container>
-  </div>
-</template>
+<template> </template>
 
 <script>
-  import Panel from '~/components/Panel';
-  import Video__container from "~/components/Video__container";
+export default {
 
-  export default {
-    components: {
-      video__container: Video__container,
-      panel: Panel,
-    },
-    data() {
-      return {
-        lessons: 'Hello',
-      };
-    },
-  };
+  },
+};
 </script>
 
-<style>
-  /*.container__panel {*/
-  /*  width: 80%;*/
-  /*  margin: 40px auto;*/
-  /*}*/
-
-</style>
+<style scoped></style>

904e34d3502bedb27dd610875793a938ff6d6174
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2020-05-09 15:34:42 +0300

    clear index

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,10 @@
-<!--TODO убрать из шаблона кнопку-->
-<template>
-  <div>
-    <arrow-button></arrow-button>
-  </div>
-</template>
+<template> </template>
 
 <script>
-import SliderButton from '@/components/ui/SliderButton';
 export default {
-  components: {
-    'arrow-button': SliderButton,
+
   },
 };
 </script>
-<!--TODO убрать див ниже-->
-<style scoped>
-div {
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #613a93;
-}
-</style>
+
+<style scoped></style>

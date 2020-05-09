e6071937c1ea9ba6d6e886b736132ef72e9f3ab8
Ekaterina Zaytseva <zajceva.katerina@gmail.com>
2020-05-09 10:02:19 +0300

    feat: add components for section Stories

---
+++ components/Stories.vue
@@ -0,0 +1,106 @@
+<template>
+  <section class="stories">
+    <ul class="stories__list">
+      <li class="stories__item" v-for="story in stories" :key="story.id">
+        <story-item
+          :author="story.author"
+          :text="story.text"
+          :photoUrl="story.photoUrl"
+          :link="`/stories/${story.id}`"
+        />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import StoryItem from '@/components/StoryItem';
+
+export default {
+  components: {
+    'story-item': StoryItem,
+  },
+  data() {
+    return {
+      stories: [
+        {
+          id: 1,
+          photoUrl: '/images/stories/story-photo1.jpg',
+          // TODO: уменьшить размеры фотографий
+          author: 'Владимир Тен',
+          text:
+            'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+        },
+        {
+          id: 2,
+          photoUrl:
+            'https://static.tildacdn.com/tild6232-6166-4435-b066-393234336532/galleryFullImage-1-1.jpg',
+          author: 'Владимир Познер',
+          text: 'Я боюсь акул — и, в отличии от рака, это не лечится.',
+        },
+        {
+          id: 3,
+          photoUrl:
+            'https://static.tildacdn.com/tild6232-6166-4435-b066-393234336532/galleryFullImage-1-1.jpg',
+          author: 'Александр Тарханов',
+          text: 'Я не могу победить свою пунктуальность в отличии от рака.',
+        },
+        {
+          id: 4,
+          photoUrl:
+            'https://static.tildacdn.com/tild6232-6166-4435-b066-393234336532/galleryFullImage-1-1.jpg',
+          author: 'Александр Тарханов',
+          text: 'Я не могу победить свою пунктуальность в отличии от рака.',
+        },
+        {
+          id: 5,
+          photoUrl:
+            'https://static.tildacdn.com/tild6232-6166-4435-b066-393234336532/galleryFullImage-1-1.jpg',
+          author: 'Александр Тарханов',
+          text: 'Я не могу победить свою пунктуальность в отличии от рака.',
+        },
+        {
+          id: 6,
+          photoUrl:
+            'https://static.tildacdn.com/tild6232-6166-4435-b066-393234336532/galleryFullImage-1-1.jpg',
+          author: 'Александр Тарханов',
+          text: 'Я не могу победить свою пунктуальность в отличии от рака.',
+        },
+        {
+          id: 7,
+          photoUrl:
+            'https://static.tildacdn.com/tild6232-6166-4435-b066-393234336532/galleryFullImage-1-1.jpg',
+          author: 'Александр Тарханов',
+          text: 'Я не могу победить свою пунктуальность в отличии от рака.',
+        },
+        {
+          id: 8,
+          photoUrl:
+            'https://static.tildacdn.com/tild6232-6166-4435-b066-393234336532/galleryFullImage-1-1.jpg',
+          author: 'Александр Тарханов',
+          text: 'Я не могу победить свою пунктуальность в отличии от рака.',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.stories__list {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 300px);
+  column-gap: 40px;
+  row-gap: 70px;
+  padding: 0;
+  list-style: none;
+  justify-content: center;
+}
+
+@media screen and (max-width: 1280px) {
+  .stories__list {
+    grid-template-columns: repeat(auto-fit, 265px);
+    row-gap: 60px;
+  }
+}
+</style>
---
+++ components/StoryItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <a class="story-item__link" :href="link">
+    <img
+      :src="photoUrl"
+      :alt="`Фотография автора ${author}`"
+      class="story-item__photo"
+    />
+    <h3 class="story-item__author">{{ author }}</h3>
+    <p class="story-item__text">{{ text }}</p>
+  </a>
+</template>
+
+<script>
+export default {
+  props: ['photoUrl', 'author', 'text', 'link'],
+};
+</script>
+
+<style scoped>
+.story-item__link {
+  text-decoration: none;
+  color: #000;
+}
+
+.story-item__photo {
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+  object-position: center;
+  display: block;
+  margin: 0 0 20px;
+}
+
+.story-item__author {
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 1;
+  margin: 0 0 14px;
+  padding: 0;
+}
+
+.story-item__text {
+  font-size: 14px;
+  line-height: 1.29;
+  color: #666666;
+  margin: 0;
+  padding: 0;
+}
+
+@media screen and (max-width: 1280px) {
+  .story-item__photo {
+    height: 265px;
+    /* TODO: подумать, мб height сделать как-то поумнее */
+  }
+}
+</style>

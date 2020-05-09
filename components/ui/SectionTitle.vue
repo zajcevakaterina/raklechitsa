afa8574c41a1ce94a82d65707b6ec23dcedfab95
vitekv384 <62235183+vitekv384@users.noreply.github.com>
2020-05-09 18:21:34 +0300

    add component SectionText SectionTitle

---
+++ components/ui/SectionTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2 class="section__title">
+    <slot></slot>
+  </h2>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.section__title {
+  width: 413px;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 36px;
+}
+</style>

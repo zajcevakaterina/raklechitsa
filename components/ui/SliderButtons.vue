fdb63c882c8c1274ff9e1d28692338b01536f00a
stivman <gisma87@yandex.ru>
2020-05-12 20:45:47 +0700

    button

--- components/ui/SliderButtons.vue
+++ components/ui/SliderButtons.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="slider-buttons">
-    <button class="slider-button">
+    <button class="slider-button" @click="$emit('btnClickLeftIntro')">
       <img
         src="@/static/arrows/leftNactivearrow.png"
         alt="arrow"
         class="arrow"
       />
     </button>
-    <button class="slider-button">
+    <button class="slider-button" @click="$emit('btnClickRightIntro')">
       <img
         src="@/static/arrows/rightActivearrow.png"
         alt="arrow"
@@ -18,25 +18,22 @@
 </template>
 
 <script>
-export default {
-  //TODO сделать обработчик активности кнопки
-  methods: {
-    buttonHandler() {},
-  },
-};
+  export default {
+    //TODO сделать обработчик активности кнопки
+    methods: {
+      buttonHandler() {},
+    },
+  };
 </script>
 
 <style scoped>
-.slider-buttons {
-  margin-top: auto;
-}
 
-.slider-button {
-  border: 0;
-  cursor: pointer;
-  width: 40px;
-  height: 40px;
-  background-color: #fbfbfb;
-  padding: 0;
-}
+  .slider-button {
+    border: 0;
+    cursor: pointer;
+    width: 40px;
+    height: 40px;
+    background-color: #fbfbfb;
+    padding: 0;
+  }
 </style>

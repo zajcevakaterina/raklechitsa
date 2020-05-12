fdf7ec06883e33f7f197fd790a1d45cfd5bea51e
Ekaterina Zaytseva <zajceva.katerina@gmail.com>
2020-05-12 18:32:24 +0300

    feat: add insta new styles

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
         >
           <section-title class="insta__title">Инстаграм</section-title>
         </a>
-        <section-text
+        <section-text class="insta__text"
           >Два раза в неделю мы просматриваем все посты по хештегу
           #этонелечится. Все истории, где нет нецензурных выражений и
           запрещенного контента попадают сюда. Следите за правильным написанием
@@ -129,25 +129,35 @@ export default {
 </script>
 
 <style scoped>
+/* TODO: может, эти стили вынести в папку blocks и импортировать css? */
 .insta {
-  display: flex;
+  display: grid;
+  grid-template-columns: fit-content(340px) 1fr;
   justify-content: space-between;
   margin: 0 auto;
 }
 
 .insta__desc {
-  padding-top: 10px;
+  padding-top: 12px;
 }
 
-/* TODO: может, эти стили вынести в папку blocks и импортировать css? */
 .insta__title-link {
   color: #000;
   text-decoration: none;
+  display: inline;
   border-bottom: 2px solid #000;
 }
 
 .insta__title {
-  margin-bottom: 32px;
+  display: inline;
+}
+
+.insta__title-link:hover {
+  opacity: 0.8;
+}
+
+.insta__text {
+  margin-top: 32px;
 }
 
 .insta__cards {
@@ -158,5 +168,44 @@ export default {
   margin: 0 0 100px 60px;
   column-gap: 20px;
   row-gap: 20px;
+}
+
+@media screen and (max-width: 1024px) {
+  .insta__text {
+    margin-top: 30px;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .insta__text {
+    margin-top: 20px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .insta {
+    grid-template-columns: 1fr;
+    justify-items: center;
+  }
+
+  .insta__desc {
+    padding-top: 0px;
+  }
+
+  .insta__text {
+    margin-top: 26px;
+  }
+
+  .insta__cards {
+    display: grid;
+    grid-template-columns: repeat(4, 157px);
+    margin: 0;
+  }
+}
+
+@media screen and (max-width: 320px) {
+  .insta__text {
+    margin-top: 16px;
+  }
 }
 </style>

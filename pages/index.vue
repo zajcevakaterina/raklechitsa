2a981d040b0152b95bd40478367afe289051685b
Ekaterina Zaytseva <zajceva.katerina@gmail.com>
2020-05-13 15:13:53 +0300

    feat: add insta store

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,10 @@
     <cover />
     <intro class="root__section" />
     <stories class="root__section" />
-    <!-- <a class="more-stories__link" href="stories">
-      <stories-button class="more-stories__button root__section">Больше статей</stories-button>
-    </a> -->
 
-    <a class="more-stories-link root__section" href="stories"
-      ><p class="more-stories-link__text">Больше статей</p></a
-    >
+    <a class="more-stories-link root__section" href="stories">
+      <p class="more-stories-link__text">Больше статей</p>
+    </a>
 
     <section class="insta root__section">
       <tag-lead class="insta__cover"
@@ -78,67 +75,10 @@ export default {
     'stories-button': Button,
   },
 
-  data() {
-    return {
-      photos: [
-        {
-          id: 1,
-          instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-          instaImage:
-            'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-          author: 'gladkova989',
-        },
-        {
-          id: 2,
-          instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-          instaImage:
-            'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-          author: 'gladkova989',
-        },
-        {
-          id: 3,
-          instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-          instaImage:
-            'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-          author: 'gladkova989',
-        },
-        {
-          id: 4,
-          instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-          instaImage:
-            'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-          author: 'gladkova989',
-        },
-        {
-          id: 5,
-          instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-          instaImage:
-            'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-          author: 'gladkova989',
-        },
-        {
-          id: 6,
-          instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-          instaImage:
-            'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-          author: 'gladkova989',
-        },
-        {
-          id: 7,
-          instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-          instaImage:
-            'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-          author: 'gladkova989',
-        },
-        {
-          id: 8,
-          instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-          instaImage:
-            'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-          author: 'gladkova989',
-        },
-      ],
-    };
+  computed: {
+    photos() {
+      return this.$store.getters['insta/getPhotos'];
+    },
   },
 };
 </script>

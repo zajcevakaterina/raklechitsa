169dabd1d21c535fb946bf1b5b21dbeb9a9e002a
Константин <Kostya.Okhlynin@gmail.com>
2020-05-13 22:50:21 +0300

    Последний перед ревью,проверил всё работает и готово

--- components/FormSocial.vue
+++ components/FormSocial.vue
@@ -1,36 +1,36 @@
 <template>
-  <div>
-    <p class="social-title">Поделитесь</p>
-    <div class="social-icons">
-      <ul class="icons-list">
-        <li class="icons-item">
-          <a href="#" class="icons-link"
-            ><img src="../static/Facebook.png" class="icons-image"
+  <div class="social">
+    <p class="social__title">Поделитесь</p>
+    <div class="social__icons">
+      <ul class="icons__list">
+        <li class="icons__item">
+          <a href="#" class="icons__link"
+            ><img src="../static/Facebook.png" class="icons__image"
           /></a>
         </li>
-        <li class="icons-item">
-          <a href="#" class="icons-link"
-            ><img src="../static/Twitter.png" class="icons-image"
+        <li class="icons__item">
+          <a href="#" class="icons__link"
+            ><img src="../static/Twitter.png" class="icons__image"
           /></a>
         </li>
-        <li class="icons-item">
-          <a href="#" class="icons-link"
-            ><img src="../static/Vkontakte (VK).png" class="icons-image"
+        <li class="icons__item">
+          <a href="#" class="icons__link"
+            ><img src="../static/Vkontakte (VK).png" class="icons__image"
           /></a>
         </li>
-        <li class="icons-item">
-          <a href="#" class="icons-link"
-            ><img src="../static/Odnoklassniki (OK).png" class="icons-image"
+        <li class="icons__item">
+          <a href="#" class="icons__link"
+            ><img src="../static/Odnoklassniki (OK).png" class="icons__image"
           /></a>
         </li>
-        <li class="icons-item">
-          <a href="#" class="icons-link"
-            ><img src="../static/Instagram.png" class="icons-image"
+        <li class="icons__item">
+          <a href="#" class="icons__link"
+            ><img src="../static/Instagram.png" class="icons__image"
           /></a>
         </li>
-        <li class="icons-item">
-          <a href="#" class="icons-link"
-            ><img src="../static/YouTube.png" class="icons-image"
+        <li class="icons__item">
+          <a href="#" class="icons__link"
+            ><img src="../static/YouTube.png" class="icons__image"
           /></a>
         </li>
       </ul>
@@ -43,26 +43,26 @@ export default {};
 </script>
 
 <style scoped>
-.icons-list {
+.icons__list {
   padding: 0;
   list-style: none;
   margin: 0;
   display: flex;
 }
 
-.icons-item {
+.icons__item {
   margin-left: 20px;
 }
 
-.icons-item:first-child {
+.icons__item:first-child {
   margin-left: 0px;
 }
 
-.social-icons {
+.social__icons {
   margin: 100px 225px 60px;
 }
 
-.social-title {
+.social__title {
   font-family: Inter;
   font-style: normal;
   font-weight: 600;
@@ -73,44 +73,44 @@ export default {};
 }
 
 @media screen and (min-width: 1280px) and (max-width: 1439px) {
-  .social-icons {
+  .social__icons {
     margin: 100px 165px 60px;
   }
-  .social-title {
+  .social__title {
     margin-left: 265px;
   }
 }
 
 @media screen and (min-width: 1024px) and (max-width: 1279px) {
-  .social-icons {
+  .social__icons {
     margin: 100px 165px 60px;
   }
-  .social-title {
+  .social__title {
     margin-left: 265px;
   }
 }
 
 @media screen and (min-width: 768px) and (max-width: 1023px) {
-  .social-icons {
+  .social__icons {
     margin: 100px 58px 60px;
   }
 
-  .social-title {
+  .social__title {
     margin-left: 160px;
   }
 }
 
 @media screen and (min-width: 320px) and (max-width: 767px) {
-  .social-icons {
+  .social__icons {
     margin: 70px 0px 35px;
   }
 
-  .social-title {
+  .social__title {
     font-size: 18px;
     line-height: 21px;
     margin-left: 85px;
   }
-  .icons-image {
+  .icons__image {
     width: 30px;
   }
 }

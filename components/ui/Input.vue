09012df4d73b5a765e9599ed1ad2a17937da8932
Ekaterina Zaytseva <zajceva.katerina@gmail.com>
2020-05-20 00:41:58 +0300

    form 3 steps

--- components/ui/Input.vue
+++ components/ui/Input.vue
@@ -4,23 +4,52 @@
     <input
       :class="['input__item', `input__item_border-theme_{{borderTheme}}`]"
       :type="type"
-      :placeholder="placeholder || 'Напишите тут'"
+      :placeholder="placeholder"
       :name="name"
       :required="required"
+      v-model="content"
+      @input="getInputContent"
     />
   </label>
 </template>
 
 <script>
 export default {
-  props: [
-    'labelText',
-    'type',
-    'name',
-    'required',
-    'placeholder',
-    'borderTheme',
-  ],
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    labelText: String,
+    type: {
+      type: String,
+      default: 'text',
+    },
+    name: String,
+    required: Boolean,
+    placeholder: String,
+    borderTheme: String,
+  },
+
+  data() {
+    return {
+      content: this.value,
+    };
+  },
+
+  watch: {
+    value(newVal, oldVal) {
+      if (newVal !== this.content) {
+        this.content = newVal;
+      }
+    },
+  },
+
+  methods: {
+    getInputContent(e) {
+      this.$emit('input', this.content);
+    },
+  },
 };
 </script>
 
@@ -29,16 +58,31 @@ export default {
   display: block;
 }
 
+.input::-webkit-input-placeholder {
+  color: #666666;
+}
+
+.input::-moz-placeholder {
+  color: #666666;
+}
+
+.input:-moz-placeholder {
+  color: #666666;
+}
+
+.input:-ms-input-placeholder {
+  color: #666666;
+}
+
 .input__item {
   display: block;
   width: 100%;
   font-family: inherit;
   font-size: 18px;
   line-height: 1.33;
-  color: #666666;
   border: none;
   resize: none;
-  padding: 0 0 10px;
+  padding: 10px 0 10px 4px;
   border-bottom: 1px solid #e7e7e7;
 }
 

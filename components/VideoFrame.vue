4ec49196a67b6460034fd2fee3b570c255d532f8
Ekaterina Zaytseva <zajceva.katerina@gmail.com>
2020-05-19 16:16:36 +0300

    feat: add policy content

--- components/VideoFrame.vue
+++ components/VideoFrame.vue
@@ -22,81 +22,81 @@
 </template>
 
 <script>
-  export default {
-    props: ['url'],
-  };
+export default {
+  props: ['url'],
+};
 </script>
 
 <!--TODO доделать адаптив для видео-->
 <style scoped>
+.video {
+  max-width: 867px;
+  width: 100%;
+}
+
+.video__container {
+  width: 100%;
+  max-height: 450px;
+  position: relative;
+  padding-bottom: calc(9 * 91% / 16);
+}
+
+.video__frame {
+  border: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+
+.video__caption {
+  margin-bottom: -24px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 16px;
+  left: 0;
+  bottom: 0;
+  position: absolute;
+  color: #666666;
+}
+
+.video__link {
+  color: #666666;
+}
+
+@media screen and (max-width: 1280px) {
   .video {
-    max-width: 867px;
+    max-width: 773px;
+    max-height: 400px;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .video {
+    max-height: 314px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .video {
     width: 100%;
   }
 
   .video__container {
-    width: 100%;
-    max-height: 450px;
-    position: relative;
-    padding-bottom: calc(9 * 91% / 16);
-  }
-
-  .video__frame {
-    border: 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: block;
-    width: 100%;
-    height: 100%;
+    max-height: 300px;
   }
 
   .video__caption {
-    margin-bottom: -24px;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 12px;
-    line-height: 16px;
-    left: 0;
-    bottom: 0;
-    position: absolute;
-    color: #666666;
+    margin-bottom: -34px;
   }
+}
 
-  .video__link {
-    color: #666666;
+@media screen and (max-width: 320px) {
+  .video__container {
+    max-height: 150px;
   }
-
-  @media screen and (max-width: 1280px) {
-    .video {
-      max-width: 773px;
-      max-height: 400px;
-    }
-  }
-
-  @media screen and (max-width: 1024px) {
-    .video {
-      max-height: 314px;
-    }
-  }
-
-  @media screen and (max-width: 768px) {
-    .video {
-      width: 100%;
-    }
-
-    .video__container {
-      max-height: 300px;
-    }
-
-    .video__caption {
-      margin-bottom: -34px;
-    }
-  }
-
-  @media screen and (max-width: 320px) {
-    .video__container {
-      max-height: 150px;
-    }
-  }
+}
 </style>
--- components/ui/SliderButtons.vue
+++ components/ui/SliderButtons.vue
@@ -1,24 +1,20 @@
 <template>
   <button class="slider-button" @click="$emit('btnClickLeftIntro')">
-    <img
-      src="@/static/arrows/leftNactivearrow.png"
-      alt="arrow"
-      class="arrow"
-    />
+    <img src="@/static/arrows/leftNactivearrow.png" alt="arrow" class="arrow" />
   </button>
 </template>
 
 <script>
-  export default {};
+export default {};
 </script>
 
 <style scoped>
-  .slider-button {
-    border: 0;
-    cursor: pointer;
-    width: 40px;
-    height: 40px;
-    background-color: #fbfbfb;
-    padding: 0;
-  }
+.slider-button {
+  border: 0;
+  cursor: pointer;
+  width: 40px;
+  height: 40px;
+  background-color: #fbfbfb;
+  padding: 0;
+}
 </style>

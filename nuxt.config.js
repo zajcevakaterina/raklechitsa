35bdd5f11632b509124496c238af511a63055f93
vitekv384 <62235183+vitekv384@users.noreply.github.com>
2020-05-25 13:04:55 +0300

    fix search and story integration

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,9 @@ export default {
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
    */
-  axios: {},
+  axios: {
+    baseURL: 'https://strapi.kruzhok.io/',
+  },
   /*
    ** Build configuration
    */
--- pages/stories/index.vue
+++ pages/stories/index.vue
@@ -58,14 +58,13 @@ export default {
   computed: {
     renderStories() {
       const { stories } = this.$store.state;
-      console.log(this.texts.toLowerCase());
 
       if (this.texts !== '') {
         this.texts = this.texts.toLowerCase();
         const newTotalItems = stories.stories.filter(
           item =>
             item.author.toLowerCase().includes(this.texts) ||
-            item.quote.toLowerCase().includes(this.texts)
+            item.title.toLowerCase().includes(this.texts)
         );
 
         return newTotalItems.filter(
@@ -92,10 +91,9 @@ export default {
       const newTotalItems = stories.stories.filter(
         item =>
           item.author.toLowerCase().includes(this.texts) ||
-          item.quote.toLowerCase().includes(this.texts)
+          item.title.toLowerCase().includes(this.texts)
       );
       this.totalItems = newTotalItems.length;
-      //event.target.reset();
     },
   },
   mounted() {
@@ -108,6 +106,10 @@ export default {
         this.itemsPerPage = 16;
       }
     }
+  },
+  fetchOnServer: false,
+  async fetch({ store }) {
+    await store.dispatch('stories/fetchStories');
   },
 };
 </script>

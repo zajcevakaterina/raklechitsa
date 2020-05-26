2a7da23b3ffbea1ece3ca729c04c37fb99ead966
Ekaterina Zaytseva <zajceva.katerina@gmail.com>
2020-05-26 16:33:26 +0300

    fix: add fetch and statistics getter fix

--- store/statistics.js
+++ store/statistics.js
@@ -24,17 +24,23 @@ export const actions = {
 
 export const getters = {
   getStatistics(state) {
-    const hardCodeStat = state.statistics.map(function(stat) {
-      if (stat.id === 3) {
-        stat.oldValue = stat.oldValue ? stat.oldValue : 60;
-        stat.currentValue = stat.currentValue ? stat.currentValue : 88;
-      } else if (stat.id === 4) {
-        stat.oldValue = stat.oldValue ? stat.oldValue : 80;
-        stat.currentValue = stat.currentValue ? stat.currentValue : 55;
-      }
-      return stat;
-    });
-    // значения захардкодили, но обезопасили, если вдруг со стороны api они придут
-    return hardCodeStat;
+    return state.statistics;
   },
 };
+
+// export const getters = {
+//   getStatistics(state) {
+//     const hardCodeStat = state.statistics.map(function(stat) {
+//       if (stat.id === 3) {
+//         stat.oldValue = stat.oldValue ? stat.oldValue : 60;
+//         stat.currentValue = stat.currentValue ? stat.currentValue : 88;
+//       } else if (stat.id === 4) {
+//         stat.oldValue = stat.oldValue ? stat.oldValue : 80;
+//         stat.currentValue = stat.currentValue ? stat.currentValue : 55;
+//       }
+//       return stat;
+//     });
+//     // значения захардкодили, но обезопасили, если вдруг со стороны api они придут
+//     return hardCodeStat;
+//   },
+// };

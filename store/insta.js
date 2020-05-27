d170771c85940c4d951ec6c8fc4745512d130a7c
vitekv384 <62235183+vitekv384@users.noreply.github.com>
2020-05-27 22:45:13 +0300

    instagram integration

--- store/insta.js
+++ store/insta.js
@@ -1,68 +1,41 @@
 export const state = () => ({
-  photos: [
-    {
-      id: 1,
-      instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-      instaImage:
-        'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-      author: 'gladkova989',
-    },
-    {
-      id: 2,
-      instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-      instaImage:
-        'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-      author: 'gladkova989',
-    },
-    {
-      id: 3,
-      instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-      instaImage:
-        'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-      author: 'gladkova989',
-    },
-    {
-      id: 4,
-      instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-      instaImage:
-        'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-      author: 'gladkova989',
-    },
-    {
-      id: 5,
-      instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-      instaImage:
-        'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-      author: 'gladkova989',
-    },
-    {
-      id: 6,
-      instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-      instaImage:
-        'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-      author: 'gladkova989',
-    },
-    {
-      id: 7,
-      instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-      instaImage:
-        'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-      author: 'gladkova989',
-    },
-    {
-      id: 8,
-      instaLink: 'https://www.instagram.com/p/B9ld-ULjMiz/',
-      instaImage:
-        'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/89358381_533144827582214_421034663561425989_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=yLJ0MQHnqZwAX8U9vny&oh=5bcad22d416cdb82bb72cdf14c603833&oe=5EE3FCCD',
-      author: 'gladkova989',
-    },
-  ],
+  instagram: [],
 });
 
-export const mutations = {};
+export const mutations = {
+  setState(state, { name, value }) {
+    return (state[name] = value);
+  },
+};
+
+const getPosts = data => {
+  return data.graphql.user.edge_owner_to_timeline_media.edges.map(post => {
+    const {
+      node: { display_url, accessibility_caption, shortcode },
+    } = post;
+    return {
+      display_url,
+      accessibility_caption,
+      url: `https://www.instagram.com/p/${shortcode}`,
+    };
+  });
+};
+
+export const actions = {
+  async fetchPhotos({ commit }) {
+    const data = await this.$axios.$get(
+      'https://www.instagram.com/raklechitsa/?__a=1'
+    );
+    const instagram = getPosts(data);
+    commit('setState', {
+      name: 'instagram',
+      value: instagram,
+    });
+  },
+};
 
 export const getters = {
   getPhotos(state) {
-    return state.photos;
+    return state.instagram;
   },
 };
